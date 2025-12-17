0eec7c20075ce5a2cfa95880ed7bf995cf9906e7
Tom Gymer <tommygymer@gmail.com>
2025-12-17 14:16:19 +0000

    part 2

--- d9/src/main.rs
+++ d9/src/main.rs
@@ -1,4 +1,7 @@
-use std::fmt::Display;
+use std::{
+    collections::{HashMap, HashSet, VecDeque},
+    fmt::Display,
+};
 
 use combinatorial::Combinations;
 
@@ -71,107 +74,224 @@ fn part_1(points: Vec<Point>) {
     println!("{:?}", pair_areas.last().unwrap());
 }
 
-fn part_2(points: Vec<CompressedPoint>) {
-    // TODO: add rotation detection
+#[derive(Debug, Clone, Copy)]
+enum Space {
+    Outside,
+    Inside,
+    Boundary,
+}
+
+impl Space {
+    fn is_polygon(&self) -> bool {
+        match self {
+            Self::Outside => false,
+            Self::Inside => true,
+            Self::Boundary => true,
+        }
+    }
+}
+
+fn fill(point: Point, space: &mut Vec<Vec<Space>>) {
+    let mut stack: Vec<Point> = vec![point];
+    let mut should_continue = true;
+    while should_continue {
+        if let Some(point) = stack.pop() {
+            let space_len = space.len() as u32;
+            match space.get(point.y as usize) {
+                None => {}
+                Some(row) => {
+                    let row_len = row.len() as u32;
+                    match row.get(point.x as usize) {
+                        None => {}
+                        Some(cell) => match *cell {
+                            Space::Outside => {}
+                            Space::Boundary => {}
+                            Space::Inside => {
+                                *space
+                                    .get_mut(point.y as usize)
+                                    .unwrap()
+                                    .get_mut(point.x as usize)
+                                    .unwrap() = Space::Outside;
+                                let _ = vec![(0i32, 1i32), (0, -1), (1, 0), (-1, 0)]
+                                    .into_iter()
+                                    .map(|(x, y)| {
+                                        (
+                                            point.x.checked_add_signed(x),
+                                            point.y.checked_add_signed(y),
+                                        )
+                                    })
+                                    .filter(|(x, y)| x.is_some() && y.is_some())
+                                    .map(|(x, y)| (x.unwrap(), y.unwrap()))
+                                    .filter(|(x, y)| *x < row_len && *y < space_len)
+                                    .map(|(x, y)| stack.push(Point { x, y }))
+                                    .collect::<()>();
+                            }
+                        },
+                    }
+                }
+            }
+        } else {
+            should_continue = false;
+        }
+    }
+}
+
+fn part_2(points: Vec<CompressedPoint>) -> u64 {
+    // construct polygon
+    let mut max_x = 0;
+    let mut max_y = 0;
+    for point in &points {
+        if point.compressed.x > max_x {
+            max_x = point.compressed.x;
+        }
+        if point.compressed.y > max_y {
+            max_y = point.compressed.y;
+        }
+    }
+
+    let mut space = vec![vec![Space::Inside; max_x as usize + 3]; max_y as usize + 3];
+
+    // boundary
     let mut edges: Vec<Edge> = points
         .iter()
         .zip(points.iter().skip(1))
-        .map(|(a, b)| Edge { a: a, b: b })
+        .map(|(a, b)| Edge {
+            a: &a.compressed,
+            b: &b.compressed,
+        })
         .collect();
     edges.push(Edge {
-        a: points.last().unwrap(),
-        b: points.first().unwrap(),
+        a: &points.last().unwrap().compressed,
+        b: &points.first().unwrap().compressed,
     });
 
-    // TODO: check that each of the four corners are within the 4 closest edges in each direction
+    for edge in edges {
+        if edge.a.x == edge.b.x {
+            // goes along y
+            let min = edge.a.y.min(edge.b.y);
+            for i in 0..=edge.a.y.abs_diff(edge.b.y) {
+                space[(min + i) as usize][edge.a.x as usize] = Space::Boundary;
+            }
+        } else if edge.a.y == edge.b.y {
+            // goes along x
+            let min = edge.a.x.min(edge.b.x);
+            for i in 0..=edge.a.x.abs_diff(edge.b.x) {
+                space[edge.a.y as usize][(min + i) as usize] = Space::Boundary;
+            }
+        } else {
+            unreachable!("this shouldn't happen in the input data")
+        }
+    }
 
-    let mut pair_areas: Vec<u64> = Combinations::of_size(points.to_owned(), 2)
+    // fill outside
+    fill(Point { x: 0, y: 0 }, &mut space);
+    println!("{:?}", space);
+
+    // construct all pairs
+    let mut pair_areas: Vec<u64> = Combinations::of_size(points, 2)
         .map(|vec| {
             let mut i = vec.into_iter();
             (i.next().unwrap(), i.next().unwrap())
         })
         .filter(|(a, b)| {
-            let rect_points = [a, &Point { x: a.x, y: b.y }, b, &Point { x: b.x, y: a.y }];
-            let rect_edges = vec![
-                Edge {
-                    a: rect_points[0],
-                    b: rect_points[1],
+            let rect_points = [
+                a.compressed,
+                Point {
+                    x: a.compressed.x,
+                    y: b.compressed.y,
                 },
-                Edge {
-                    a: rect_points[1],
-                    b: rect_points[2],
-                },
-                Edge {
-                    a: rect_points[2],
-                    b: rect_points[3],
-                },
-                Edge {
-                    a: rect_points[3],
-                    b: rect_points[0],
+                b.compressed,
+                Point {
+                    x: b.compressed.x,
+                    y: a.compressed.y,
                 },
             ];
-            if true == false {
-                println!("{:?}", rect_points);
-                println!("{:?}", rect_edges);
-                println!(
-                    "{:?}",
-                    rect_edges
-                        .iter()
-                        .map(|e| edges
-                            .iter()
-                            .filter(|other| e.intersects(other))
-                            .collect::<Vec<&Edge>>())
-                        .collect::<Vec<Vec<&Edge>>>()
-                )
-            }
-            !rect_edges
-                .into_iter()
-                .any(|e| edges.iter().any(|other| e.intersects(other)))
+            let rect_edges = vec![
+                Edge {
+                    a: &rect_points[0],
+                    b: &rect_points[1],
+                },
+                Edge {
+                    a: &rect_points[1],
+                    b: &rect_points[2],
+                },
+                Edge {
+                    a: &rect_points[2],
+                    b: &rect_points[3],
+                },
+                Edge {
+                    a: &rect_points[3],
+                    b: &rect_points[0],
+                },
+            ];
+
+            rect_edges.into_iter().all(|edge| {
+                if edge.a.x == edge.b.x {
+                    // goes along y
+                    let min = edge.a.y.min(edge.b.y);
+                    (0..=edge.a.y.abs_diff(edge.b.y))
+                        .map(|i| space[(min + i) as usize][edge.a.x as usize])
+                        .all(|s| s.is_polygon())
+                } else if edge.a.y == edge.b.y {
+                    // goes along x
+                    let min = edge.a.x.min(edge.b.x);
+                    (0..=edge.a.x.abs_diff(edge.b.x))
+                        .map(|i| space[edge.a.y as usize][(min + i) as usize])
+                        .all(|s| s.is_polygon())
+                } else {
+                    unreachable!("this shouldn't happen in the input data")
+                }
+            })
         })
         .map(|(a, b)| a.rect_area(&b))
         .collect();
 
     pair_areas.sort();
 
-    println!("{:?}", pair_areas.last().unwrap());
+    *pair_areas.last().unwrap()
 }
 
 fn main() {
     let input = std::fs::read_to_string("input.txt").unwrap();
 
-    let mut points: Vec<(usize, Point, Option<u32>, Option<u32>)> = input
+    let points: Vec<Point> = input
         .split('\n')
         .filter(|line| line.len() > 0)
-        .enumerate()
-        .map(|(i, line)| (i, Point::from(line), None, None))
+        .map(|line| Point::from(line))
         .collect();
 
-    points.sort_by_key(|p| p.1.x);
-    points
-        .iter_mut()
+    let xs: HashSet<u32> = points.iter().map(|p| p.x).collect();
+    let mut xs: Vec<u32> = xs.into_iter().collect();
+    xs.sort();
+    let xs: HashMap<u32, u32> = xs
+        .into_iter()
         .enumerate()
-        .map(|(i, (_, _, x, _))| *x = Some(i as u32))
-        .collect::<()>();
-    points.sort_by_key(|p| p.1.y);
-    points
-        .iter_mut()
+        .map(|(i, x)| (x, (i + 1) as u32))
+        .collect();
+
+    let ys: HashSet<u32> = points.iter().map(|p| p.y).collect();
+    let mut ys: Vec<u32> = ys.into_iter().collect();
+    ys.sort();
+    let ys: HashMap<u32, u32> = ys
+        .into_iter()
         .enumerate()
-        .map(|(i, (_, _, _, y))| *y = Some(i as u32))
-        .collect::<()>();
-    points.sort_by_key(|p| p.0);
+        .map(|(i, y)| (y, (i + 1) as u32))
+        .collect();
 
     let points: Vec<CompressedPoint> = points
         .into_iter()
-        .map(|(_, p, x, y)| CompressedPoint {
+        .map(|p| CompressedPoint {
             original: p,
             compressed: Point {
-                x: x.unwrap(),
-                y: y.unwrap(),
+                x: *xs.get(&p.x).unwrap(),
+                y: *ys.get(&p.y).unwrap(),
             },
         })
         .collect();
 
-    part_2(points);
+    let res = part_2(points);
+
+    println!("{}", res);
 }
 
 #[cfg(test)]
