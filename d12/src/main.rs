272e197708b8f8e7bc932fe1ab84ca85b8ccfcde
Tom Gymer <tommygymer@gmail.com>
2025-12-17 16:17:58 +0000

    shape parser

--- d12/src/main.rs
+++ d12/src/main.rs
@@ -1,3 +1,32 @@
+struct Shape {
+    shape: [[bool; 3]; 3],
+}
+
+impl From<&Vec<&str>> for Shape {
+    fn from(value: &Vec<&str>) -> Self {
+        let mut shape = [[false; 3]; 3];
+        for i in 0..3 {
+            for j in 0..3 {
+                shape[i][j] = if value
+                    .get(i)
+                    .unwrap()
+                    .chars()
+                    .into_iter()
+                    .collect::<Vec<char>>()
+                    .get(j)
+                    .unwrap()
+                    == &'#'
+                {
+                    true
+                } else {
+                    false
+                }
+            }
+        }
+        Self { shape }
+    }
+}
+
 fn main() {
     let input = std::fs::read_to_string("example.txt").unwrap();
 }
