2619d3dbf990a8d7a34ecb5363b0f2182ce234b6
Tom Gymer <tommygymer@gmail.com>
2026-05-28 13:07:21 +0100

    brute force

--- y2015/d19/src/main.rs
+++ y2015/d19/src/main.rs
@@ -1,9 +1,66 @@
-use std::collections::{HashMap, HashSet};
+use std::collections::HashSet;
 
 #[derive(Debug, Clone)]
 struct Rule {
     from: String,
     to: String,
+}
+
+fn generate_molecules(rules: &Vec<Rule>, medicine: &str) -> HashSet<String> {
+    let mut new: HashSet<String> = HashSet::new();
+
+    for (i, c) in medicine.char_indices() {
+        for rule in rules {
+            if rule.from.len() == 1 {
+                if rule.from.chars().next().unwrap() == c {
+                    let mut mut_medicine = medicine.to_string();
+                    mut_medicine.replace_range(i..i + 1, &rule.to);
+                    new.insert(mut_medicine);
+                }
+            }
+        }
+    }
+
+    let mut prev = None;
+    for (i, c) in medicine.char_indices() {
+        if let Some(p) = prev {
+            for rule in rules {
+                if rule.from.len() == 2 {
+                    let mut rule_from_iter = rule.from.chars();
+                    if rule_from_iter.next().unwrap() == p && rule_from_iter.next().unwrap() == c {
+                        let mut mut_medicine = medicine.to_string();
+                        mut_medicine.replace_range(i - 1..i + 1, &rule.to);
+                        new.insert(mut_medicine);
+                    }
+                }
+            }
+        }
+        prev = Some(c);
+    }
+
+    new
+}
+
+fn find_shortest_generator(
+    rules: &Vec<Rule>,
+    target: &str,
+    molecule: &str,
+    applied_rules: usize,
+) -> Option<usize> {
+    if molecule.len() > target.len() {
+        None
+    } else {
+        if molecule == target {
+            Some(applied_rules)
+        } else {
+            generate_molecules(rules, molecule)
+                .iter()
+                .filter_map(|molecule| {
+                    find_shortest_generator(rules, target, molecule, applied_rules + 1)
+                })
+                .min()
+        }
+    }
 }
 
 fn main() {
@@ -14,13 +71,13 @@ fn main() {
 
     let lines: Vec<&str> = input.split("\n").collect();
 
-    let mut medcine: String = String::from("");
+    let mut medicine: String = String::from("");
     let mut rules: Vec<Rule> = vec![];
 
     let mut found_empty = false;
     for line in lines {
         if found_empty {
-            medcine = line.trim().to_owned();
+            medicine = line.trim().to_owned();
         } else if line.len() == 0 {
             found_empty = true;
         } else {
@@ -32,39 +89,12 @@ fn main() {
         }
     }
 
-    println!("Rules: {:#?}", rules);
-    println!("Medicine: {}", medcine);
+    // println!("Rules: {:#?}", rules);
+    // println!("Medicine: {}", medicine);
 
-    let mut new: HashSet<String> = HashSet::new();
-
-    for (i, c) in medcine.char_indices() {
-        for rule in &rules {
-            if rule.from.len() == 1 {
-                if rule.from.chars().next().unwrap() == c {
-                    let mut mut_medicine = medcine.clone();
-                    mut_medicine.replace_range(i..i + 1, &rule.to);
-                    new.insert(mut_medicine);
-                }
-            }
-        }
-    }
-
-    let mut prev = None;
-    for (i, c) in medcine.char_indices() {
-        if let Some(p) = prev {
-            for rule in &rules {
-                if rule.from.len() == 2 {
-                    let mut rule_from_iter = rule.from.chars();
-                    if rule_from_iter.next().unwrap() == p && rule_from_iter.next().unwrap() == c {
-                        let mut mut_medicine = medcine.clone();
-                        mut_medicine.replace_range(i - 1..i + 1, &rule.to);
-                        new.insert(mut_medicine);
-                    }
-                }
-            }
-        }
-        prev = Some(c);
-    }
+    let new = generate_molecules(&rules, &medicine);
 
     println!("{}", new.into_iter().count());
+
+    println!("{:?}", find_shortest_generator(&rules, &medicine, "e", 0));
 }
